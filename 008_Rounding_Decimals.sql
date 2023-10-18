45832032f8d3760a16251bc29d2c1e1ab489915d
ivansukhoparov <rbcdaaa@gmail.com>
2023-10-18 03:34:53 +0200

    done: -SQL Basics: Simple WHERE and ORDER BY- SQL codewars.com task is done

---
+++ 008_Rounding_Decimals.sql
@@ -0,0 +1,16 @@
+/*
+
+Easy SQL: Rounding Decimals
+
+Given the following table 'decimals':
+
+** decimals table schema **
+
+id
+number1
+number2
+Return a table with two columns (number1, number2), the value in number1 should be rounded down and the value in number2 should be rounded up.
+*/
+SELECT FLOOR(number1) AS number1, --accepts a fractional number and returns the maximum integer not less than the accepted one
+ CEILING(number2) AS number2 --accepts a fractional number and returns the maximum integer not grater than the accepted one
+FROM decimals
