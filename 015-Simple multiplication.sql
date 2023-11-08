4e8826fe662fb10cc2ea0117880731a2a5df4b16
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-08 17:14:38 +0100

    done: 'Simple multiplication'  task for SQL  on codewars

---
+++ 015-Simple multiplication.sql
@@ -0,0 +1,11 @@
+/*
+
+Simple multiplication
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+SELECT number, CASE number%2
+                   WHEN 0 THEN number*8
+                   ELSE number*9
+               END AS res
+FROM multiplication
