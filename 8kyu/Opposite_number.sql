20434764124749d61a1b4879ebedfda9da517749
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-26 13:13:57 +0100

    -Opposite number- move to kyu specify folder rename file

---
+++ 8kyu/Opposite_number.sql
@@ -0,0 +1,20 @@
+--
+-- Opposite number
+--
+-- Very simple, given an integer or a floating-point number, find its opposite.
+--
+-- You will be given a table: "opposite", with a column: "number". Return a table with a column: "res".
+-- At first request add new column in table "opposite", named "res", without default value
+--
+
+ALTER TABLE opposite ADD res INTEGER NULL;
+
+-- After "res" column - assign "res" value as "number" multiply by -1
+
+UPDATE opposite
+SET res = number*(-1);
+
+-- Output only "res" value from table "opposite"
+
+SELECT res
+FROM opposite
