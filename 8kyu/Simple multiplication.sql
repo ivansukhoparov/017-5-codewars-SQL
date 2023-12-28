f18ac7a72a9b27ecda2ac59d46ea23e85a24297e
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-28 13:25:17 +0100

    "Simple multiplication" move to kyu specify folder rename file

--- 8kyu/Simple multiplication.sql
+++ 8kyu/Simple multiplication.sql
@@ -1,9 +1,9 @@
-/*
+-- 
+-- Simple multiplication
+-- 
+-- This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+-- 
 
-Simple multiplication
-
-This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
-*/
 SELECT number, CASE number%2
                    WHEN 0 THEN number*8
                    ELSE number*9
